7edbd1dfaf75e0e1caebee86132dfb30719bb100
voyager <cdspacenoob@gmail.com>
2023-01-16 17:23:03 +0900

    StyledComps,TTL

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,20 +5,38 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  // 입력된 값의 유효성을 체크하기 위한 state.
+  // 유효하지 않을 때 flag를 false로 정하고, 유효할 때는 true로 정할 것이다. 
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    console.log(enteredValue.length);
+    // 입력받은 값을 trim하여 그 길이를 체크한다.
+    if (enteredValue.length === 0) {
+      // 입력된 값이 없다면 유효하지 않은 것이다. state를 false로. 
+      setIsValid(false);
+      // 아무 것도 입력되지 않았다면 return으로 흐름을 종료하여 공백 입력이 일어나지 않게 한다.
+      // alert도 작동한다. 
+      // alert("no data inserted");
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        {/* {}를 사용하여 동적으로 스타일을 적용할 수 있다. */}
+        <label>Course Goal
+        </label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// 아래 styled.button`` 이런 구조를 태그드 템플릿 리터럴이라고 부른다. 
+// 버튼 메소드에 접근한 상태다. ()는 없지만 메소드다. 
+const Button = styled.button`
+
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+&:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #ac0e77;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+`;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
